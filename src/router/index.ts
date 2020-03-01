6a8e04434aab70bb2be79de2792eb7455618aed9
garcuw <arcuw74@gmail.com>
2020-03-01 15:42:53 +0900

    Rab4

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Table from '../views/Table.vue'
+import Form from '../views/Form.vue'
 
 Vue.use(VueRouter)
 
@@ -16,7 +18,18 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/table',
+    name: 'table',
+    component: Table
+  },
+  {
+    path: '/form',
+    name: 'form',
+    component: Form
   }
 ]
 
